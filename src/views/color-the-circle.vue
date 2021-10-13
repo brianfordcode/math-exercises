e1dfd2b61d4d99a18c1e99220ad890344d435a39
Brian Ford <brianfordcode@gmail.com>
2021-10-13 15:13:05 -0700

    basic color circle game complete

--- src/views/color-the-circle.vue
+++ src/views/color-the-circle.vue
@@ -1,54 +1,102 @@
 <template>
-  <h1>color the circle</h1>
 
-
-    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg">
         <circle class="circle"
           v-for="(circle, index) in circles"
           :key="index"
-          @click="circle.fill = circle.fill === 'black' ? 'orange' : 'black'"
+          @click="circle.fill = circle.fill === 'red' ? 'green' : 'red', colorIn(index)"
           :cx="circle.x"
           :cy="circle.y"
           :r="circle.r"
           :fill="circle.fill"
-          stroke="black"
-          stroke-width="3"
         />
     </svg>
 
+    <button @click="getNewNumber">Get a Number!</button>
+
+    <h1 v-if="gameStart">Select circle<span>{{ randNum }}</span></h1>
 
 </template>
 
 <script>
+import Swal from "sweetalert2";
+
+const generateCircles = nCircles => {
+    const circles = []
+    const width = 300
+    const r = 20
+    const spacing = width/nCircles
+    for (let i=0; i < nCircles; i++) {
+        circles.push({
+            x: spacing * (i + 0.5),
+            y: 50,
+            r,
+            fill: 'green'
+        })
+    }
+    return circles
+}
+
 export default {
     data() {
-        const nCircles = 5
-        const circles = []
-        const width = 300
-        const r = 20
-        const spacing = width/nCircles
-        for (let i=0; i < nCircles; i++) {
-            circles.push({
-                x: spacing * (i + 0.5),
-                y: 50,
-                r,
-                fill: 'orange'
-            })
-        }
+        const randNum = Math.floor(Math.random() * 5) + 1
         return {
-            circles
+            circles: generateCircles(5),
+            randNum,
+            gameStart: false,
         }
     },
     methods: {
+        colorIn(index) {
+            let selectedNum = index + 1
+            if (this.gameStart) {
+                if (selectedNum === this.randNum) {
+                    Swal.fire("Correct!");
+                    this.circles = generateCircles(5)
+                    this.randNum = Math.floor(Math.random() * 5) + 1
+                } else {
+                    Swal.fire("Try Again!");
+                }
+            }
+        },
+        getNewNumber() {
+            this.gameStart = true
+            this.randNum = Math.floor(Math.random() * 5) + 1
+        },
     }
 
 }
 </script>
 
-<style>
+<style scoped>
 
 .circle {
     cursor: pointer;
+}
+
+button {
+  outline: none;
+  border: none;
+  color: white;
+  background-color: darkblue;
+  padding: 20px;
+  margin: 0 20px;
+  cursor: pointer;
+}
+button:active {
+  transform: scale(0.95);
+}
+
+h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 20px;
+}
+
+span {
+    padding-left: 10px;
+    font-size: 50px;
 }
 
 </style>
